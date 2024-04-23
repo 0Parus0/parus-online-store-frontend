30f476983943871270d10bc4c7a4eabf53e25e56
parus <0Parus0@gmail.com>
2024-04-23 02:31:14 +0100

    initial commit

---
+++ src/pages/admin/apps/coupon.tsx
@@ -0,0 +1,145 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import { FormEvent, useEffect, useState } from "react";
+import AdminSidebar from "../../../components/admin/AdminSidebar";
+import { RootState } from "../../../redux/store";
+import axios from "axios";
+import { server } from "../../../redux/store";
+import { useSelector } from "react-redux";
+
+const allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const allNumbers = "1234567890";
+const allSymbols = "!@#$%^&*()_+";
+
+const Coupon = () => {
+  const { user } = useSelector((state: RootState) => state.userReducer);
+  const [size, setSize] = useState<number>(8);
+  const [amount, setAmount] = useState<number>(0);
+  const [prefix, setPrefix] = useState<string>("");
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
+  const [includeCharacters, setIncludeCharacters] = useState<boolean>(false);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const [coupon, setCoupon] = useState<string>("");
+
+  const copyText = async (coupon: string) => {
+    await window.navigator.clipboard.writeText(coupon);
+    setIsCopied(true);
+  };
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!includeNumbers && !includeCharacters && !includeSymbols)
+      return alert("Please Select One At Least");
+
+    let result: string = prefix || "";
+    const loopLength: number = size - result.length;
+
+    for (let i = 0; i < loopLength; i++) {
+      let entireString: string = "";
+      if (includeCharacters) entireString += allLetters;
+      if (includeNumbers) entireString += allNumbers;
+      if (includeSymbols) entireString += allSymbols;
+
+      const randomNum: number = ~~(Math.random() * entireString.length);
+      result += entireString[randomNum];
+    }
+    try {
+      await axios.post(
+        `${server}/api/v1/payment/coupon/new?id=${user?._id!}`,
+        {
+          amount,
+          coupon: result,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+
+    setCoupon(result);
+  };
+
+  useEffect(() => {
+    setIsCopied(false);
+  }, [coupon]);
+
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main className="dashboard-app-container">
+        <h1>Coupon</h1>
+        <section>
+          <form className="coupon-form" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Text to include"
+              value={prefix}
+              onChange={(e) => setPrefix(e.target.value)}
+              maxLength={size}
+            />
+
+            <input
+              type="number"
+              placeholder="Coupon Length"
+              value={size}
+              onChange={(e) => setSize(Number(e.target.value))}
+              min={8}
+              max={25}
+            />
+            <label htmlFor="amount">Please Enter the coupon amount</label>
+            <input
+              id="amount"
+              type="number"
+              placeholder="Enter amount"
+              value={amount}
+              onChange={(e) => setAmount(Number(e.target.value))}
+            />
+
+            <fieldset>
+              <legend>Include</legend>
+
+              <input
+                type="checkbox"
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers((prev) => !prev)}
+              />
+              <span>Numbers</span>
+
+              <input
+                type="checkbox"
+                checked={includeCharacters}
+                onChange={() => setIncludeCharacters((prev) => !prev)}
+              />
+              <span>Characters</span>
+
+              <input
+                type="checkbox"
+                checked={includeSymbols}
+                onChange={() => setIncludeSymbols((prev) => !prev)}
+              />
+              <span>Symbols</span>
+            </fieldset>
+            <button type="submit">Generate</button>
+          </form>
+
+          {coupon && (
+            <code>
+              {coupon}{" "}
+              <span onClick={() => copyText(coupon)}>
+                {isCopied ? "Copied" : "Copy"}
+              </span>{" "}
+            </code>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Coupon;
